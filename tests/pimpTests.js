1d4b1f290e8c431a31a6fd4e0ecc75f88ad35760
Mudit Ameta <zeusdeux@gmail.com>
2014-05-18 12:14:05 +0530

    fixed formatting

--- tests/pimpTests.js
+++ tests/pimpTests.js
@@ -11,7 +11,8 @@ else {
   console.log("  <p style='margin:10px 60px;display: inline-block;margin-top: 15px;border: 1px solid #EEE; \
               padding: 10px;-webkit-box-shadow: inset 0 0 2px #EEE;-moz-box-shadow: inset 0 0 2px #eee; \
               box-shadow: inset 0 0 2px #EEE;-webkit-border-radius: 5px;-moz-border-radius: 5px;\
-              border-radius: 5px;color: #1BA2D6;font-size: 1.2em;'>Using wrap.js and pimp.js from: " + PIMP_LIB_DIR.split("/").splice(0,2).join("/") + "</p>");
+              border-radius: 5px;color: #1BA2D6;font-size: 1.2em;'>Using wrap.js and pimp.js from: " 
+              + PIMP_LIB_DIR.split("/").splice(0,2).join("/") + "</p>");
 }
 
 describe("pimp", function() {
